75421ff76a742f3949e866805c786b0ff8a608c7
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-12 17:31:56 +0200

    Add for and querySelector

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+/* MILESTONE 2
+Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile. Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript. */
+
+// Creare un array con le immagini
 const images = [
     './assets/img/01.webp',
     './assets/img/02.webp',
@@ -6,4 +10,13 @@ const images = [
     './assets/img/05.webp'
 ];
 
-console.log(images[1])
+//Selezioniamo il div dove vogliamo inserire le immagini
+const slidesEl = document.querySelector('.slides');
+
+//Crea un ciclo for con le immagini
+for (let i= 0; i < images.length; i++) {
+    const imagesEl = images[i];
+    //Stampo su console tutte le immagini
+    console.log(imagesEl);
+    
+}
